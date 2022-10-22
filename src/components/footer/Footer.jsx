633f76bc6c7d53f9c878b28573bb96a08ea18ea7
saugat <saugatrajbhandari47@gmail.com>
2022-10-22 08:10:07 +0545

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <p className="text-center mt-10 bg-primary py-3 text-white">
+      Copyright &copy; 2022. All right reserved.
+    </p>
+  );
 };
 
 export default Footer;
